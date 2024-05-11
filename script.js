dd9ed0dd9e8d01b17e095e63491f39ffc2f012f3
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-05-11 10:27:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+let input = document.getElementById("input-id");
+let color = document.getElementById("color-id");
+let btn = document.querySelector("button");
+
+
+let gridElements = document.querySelectorAll(".box");
+
+
+
+
+
+btn.addEventListener("click",function updateGrid(e){
+    e.preventDefault();
+    let inputValue = input.value;
+    let colorValue = color.value;
+
+    gridElements.forEach(element => {
+        if(element.id !== inputValue){
+            element.style.backgroundColor ="white";
+        }
+        else{
+            element.style.backgroundColor =`${colorValue}`;
+        }
+    });
+    
+   
+    
+
+});
